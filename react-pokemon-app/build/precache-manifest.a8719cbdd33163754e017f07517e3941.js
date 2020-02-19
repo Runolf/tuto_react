f7178224f78c5bf74a5ebf6431b3bc281f491d51
Runolf <38182335+Runolf@users.noreply.github.com>
2020-02-19 09:43:53 +0100

    add npm start build

---
+++ react-pokemon-app/build/precache-manifest.a8719cbdd33163754e017f07517e3941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1aa254ec5fcf44351b1d08185f5d39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3c35fb376fe02de1b5",
+    "url": "/static/css/main.ad702741.chunk.css"
+  },
+  {
+    "revision": "38f00b54ef5e061e827d",
+    "url": "/static/js/2.c456fe4f.chunk.js"
+  },
+  {
+    "revision": "7b3c35fb376fe02de1b5",
+    "url": "/static/js/main.ecca12eb.chunk.js"
+  },
+  {
+    "revision": "0ccfaae032ceb2a13aa8",
+    "url": "/static/js/runtime-main.5f5fd4e6.js"
+  }
+]);
